941e902207017c5d81e679d88e3d2c030c80f7d3
yamacraft <bmfk87@gmail.com>
2014-04-28 16:15:57 +0900

    Feed読み込み追加

---
+++ js/feed.js
@@ -0,0 +1,33 @@
+/*
+ * http://www.jdev.it/a-very-simple-rss-reader-with-angularjs-and-google-feed-api/
+ */
+var feeds = [];
+angular.module('feedModule', ['ngResource'])
+    .factory('FeedLoader', function ($resource) {
+        return $resource('http://ajax.googleapis.com/ajax/services/feed/load', {}, {
+            fetch: { method: 'JSONP', params: {v: '1.0', callback: 'JSON_CALLBACK'} }
+        });
+    })
+    .service('FeedList', function ($rootScope, FeedLoader) {
+        this.get = function() {
+            var feedSources = [
+                {title: 'Qiita',  url: 'http://qiita.com/yamacraft/feed'},
+                {title: 'exblog', url: 'http://yamacraft2.exblog.jp/index.xml'},
+                ];
+            if (feeds.length === 0) {
+                for (var i=0; i<feedSources.length; i++) {
+                    FeedLoader.fetch({q: feedSources[i].url, num: 10}, {}, function (data) {
+                        var feed = data.responseData.feed;
+                        feeds.push(feed);
+                    });
+                }
+            }
+            return feeds;
+        };
+    })
+    .controller('FeedCtrl', function ($scope, FeedList) {
+        $scope.feeds = FeedList.get();
+        $scope.$on('FeedList', function (event, data) {
+            $scope.feeds = data;
+        });
+    });
